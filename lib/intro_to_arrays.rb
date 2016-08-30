953311eefc5c61fa6f36fd6f33c4833478c0f583
RKoopman <raoul.koopman@gmail.com>
2016-08-30 16:59:43 -0400

    fun with arrays

---
+++ lib/intro_to_arrays.rb
@@ -0,0 +1,31 @@
+def instantiate_new_array
+  ary1 = []
+end
+
+def array_with_two_elements
+  ary2 = [ "1", "2" ]
+end
+
+def first_element(ary)
+  ary.first
+end
+
+def third_element(ary)
+  ary[2]
+end
+
+def last_element(ary)
+  ary[-1]
+end
+
+def first_element_with_array_methods(ary)
+  first_element(ary)
+end
+
+def last_element_with_array_methods(ary)
+  last_element(ary)
+end
+
+def length_of_array(ary)
+  ary.length
+end
